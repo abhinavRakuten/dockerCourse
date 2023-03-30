c39f96c4ba732f3129ae34457b2a008c655d3e61
abhinavjalawadi <abhinav.jalawadi@rakuten.com>
2023-03-30 11:00:47 +0530

    added a feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi there,  just changing stuff here to be pushed
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hi there, this code is coming from feature branch</p>
     </div>
   );
 }
